ead40b0db5a6a031053029a6e4171324dcab5eb8
AEMLoviji <AEMLoviji@gmail.com>
2019-10-10 23:02:01 +0300

    feat: category service injected to category controller

--- src/SuperMarket.API/Controllers/CategoriesController.cs
+++ src/SuperMarket.API/Controllers/CategoriesController.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SuperMarket.API.Domain.Models;
+using SuperMarket.API.Domain.Services;
 
 namespace SuperMarket.API.Controllers
 {
@@ -6,5 +10,18 @@ namespace SuperMarket.API.Controllers
     [ApiController]
     public class CategoriesController : Controller
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            return categories;
+        }
     }
 }
