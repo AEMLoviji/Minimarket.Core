554e63f3b99830706f206f70dc67664d3658ea2e
AEMLoviji <AEMLoviji@gmail.com>
2019-10-25 00:03:27 +0300

    feat: added delete category action

--- src/Minimarket.API/Domain/Repositories/CategoryRepository.cs
+++ src/Minimarket.API/Domain/Repositories/CategoryRepository.cs
@@ -15,7 +15,9 @@ namespace Minimarket.API.Domain.Repositories
         public async Task AddAsync(Category category) => await _context.Categories.AddAsync(category);
 
         public async Task<Category> FindByIdAsync(int id) => await _context.Categories.FindAsync(id);
-        
+
         public void Update(Category category) => _context.Categories.Update(category);
+
+        public void Remove(Category category) => _context.Categories.Remove(category);
     }
 }
