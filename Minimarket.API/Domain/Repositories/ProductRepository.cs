3d788ae7822e86fd71244e1caca2f070c78d6aea
AEMLoviji <AEMLoviji@gmail.com>
2019-10-27 01:55:47 +0300

    feat: append related category object to product model

--- Minimarket.API/Domain/Repositories/ProductRepository.cs
+++ Minimarket.API/Domain/Repositories/ProductRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Minimarket.API.Domain.Db.Contexts;
 using Minimarket.API.Domain.Models;
@@ -10,6 +11,8 @@ namespace Minimarket.API.Domain.Repositories
     {
         public ProductRepository(AppDbContext context) : base(context) { }
 
-        public async Task<IEnumerable<Product>> ListAsync() => await _context.Products.ToListAsync();
+        public async Task<IEnumerable<Product>> ListAsync() =>
+            await _context.Products.Include(p => p.Category)
+                                   .ToListAsync();
     }
 }
